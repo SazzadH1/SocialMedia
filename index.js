d328d85d24ef7dfc1f7abbd45b8b7423e1f584d1
Sazzad Hossain <sazzad.hossain@pipedrive.com>
2022-07-31 21:30:45 +0300

    updated code again

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const Koa= require ('koa');
 const app = new Koa();
 
+const bodyParser = require("koa-bodyparser");
+app.use(bodyParser());
+
 const router = require('./lib/routes/socialmedia-router');
 
 app.use(router.routes());
+
+
 
 app.listen (3001, () => {
 console.log('Server running at http://localhost:3001');
